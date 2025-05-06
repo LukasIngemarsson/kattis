4a33eb3c29458b140e8b9a9ae16c7df5a2d2e7cd
lukin921 <lukin921@student.liu.se>
2025-05-06 23:56:38 +0200

    +1

---
+++ 3d-printed-statues/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(false);
+
+    int n; cin >> n;
+    cout << ceil(log2(n)) + 1 << '\n';
+
+    return 0;
+}
